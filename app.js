ec0678d734613d6cc93f42144647e6b1d15972fb
Jay Patel <jjpatel361@users.noreply.github.com>
2020-08-19 17:22:35 -0400

    Fix : my noob ness
    This was just a raw commit for keeping this in Github.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var twilio = require('twilio');
 var http = require('http');
-var accountSid = 'AC39276feef00008158a3392e316a7bdfe'; // Your Account SID from www.twilio.com/console
-var authToken = 'bb15952c282da95f5751a41ff089cf71';   // Your Auth Token from www.twilio.com/console
+var accountSid = ; // Your Account SID from www.twilio.com/console
+var authToken = ;   // Your Auth Token from www.twilio.com/console
 
 var twilio = require('twilio');
 var client = new twilio.RestClient(accountSid, authToken);
